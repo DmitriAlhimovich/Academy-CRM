0c16234c17eeaafbb1d24db15a59b4f4473db9c0
Dmitriy Alhimovich <dmitriy.alhimovich@arcadia.spb.ru>
2021-08-20 10:18:56 +0300

    Remove unused project

--- AcademyCRM.Web.Mvc/Models/StudentModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AcademyCRM.Web.Mvc.Models
-{
-    public class StudentModel : PersonModel
-    {
-        public int GroupId { get; set; }
-        public StudentGroupModel Group { get; set; }
-    }
-}
