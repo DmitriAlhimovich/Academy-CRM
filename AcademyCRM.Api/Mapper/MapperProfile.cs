e64305c3668bc3ce9ed3fc718916599ee8add2dc
Dmitriy Alhimovich <dmitriy.alhimovich@arcadia.spb.ru>
2021-08-23 12:59:25 +0300

    Add WebApi project

---
+++ AcademyCRM.Api/Mapper/MapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AcademyCRM.Api.Dto;
+using AcademyCRM.BLL.Models;
+using AutoMapper;
+
+namespace AcademyCRM.Api.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Course, CourseDto>()
+                .ForMember(model => model.TopicName, map => map.MapFrom(c => c.Topic.Title))
+                .ReverseMap();
+        }
+    }
+}
