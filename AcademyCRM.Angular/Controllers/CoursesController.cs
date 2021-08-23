b7e5e557ff29e7662a994a191595c79d7f86b01f
Dmitriy Alhimovich <dmitriy.alhimovich@arcadia.spb.ru>
2021-08-23 13:23:27 +0300

    Angular project added

---
+++ AcademyCRM.Angular/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using AcademyCRM.Angular.Dto;
+using AcademyCRM.BLL.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcademyCRM.Angular.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseService _service;
+        private readonly IMapper _mapper;
+
+        public CoursesController(ICourseService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IEnumerable<CourseDto> Get()
+        {
+            return _mapper.Map<IEnumerable<CourseDto>>(_service.GetAll());
+        }
+    }
+}
---
+++ AcademyCRM.Angular/Mapper/MapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AcademyCRM.Angular.Dto;
+using AcademyCRM.BLL.Models;
+using AutoMapper;
+
+namespace AcademyCRM.Angular.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Course, CourseDto>()
+                .ForMember(model => model.TopicName, map => map.MapFrom(c => c.Topic.Title))
+                .ReverseMap();
+        }
+    }
+}
