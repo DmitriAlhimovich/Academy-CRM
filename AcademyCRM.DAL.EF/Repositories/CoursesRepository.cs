f29c6cd19315cd9acd2ef276f2fffea510c2458e
Dmitriy Alhimovich <dmitriy.alhimovich@arcadia.spb.ru>
2021-08-23 14:39:07 +0300

    Add courses component

---
+++ AcademyCRM.DAL.EF/Repositories/CoursesRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AcademyCRM.BLL.Models;
+using AcademyCRM.DAL.EF.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcademyCRM.DAL.EF.Repositories
+{
+    public class CoursesRepository : BaseRepository<Course>
+    {
+        private readonly AcademyContext _context;
+
+        public CoursesRepository(AcademyContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public override IEnumerable<Course> GetAll()
+        {
+            return _context.Courses
+                .Include(c => c.Topic)
+                .ToList();
+        }
+    }
+}
