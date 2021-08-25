df4651676d77e3e81e08072b10f49a6addcfb59a
Dmitriy Alhimovich <dmitriy.alhimovich@arcadia.spb.ru>
2021-08-25 12:04:53 +0300

    apply filter onClick

--- AcademyCRM.Angular/ClientApp/src/app/courses/courses.component.ts
+++ AcademyCRM.Angular/ClientApp/src/app/courses/courses.component.ts
@@ -6,12 +6,20 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './courses.component.html'
 })
 export class CoursesComponent {
+  public coursesAll: Course[];
   public courses: Course[];
+  public topicId: number;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<Course[]>(baseUrl + 'courses').subscribe(result => {
+      this.coursesAll = result;
       this.courses = result;
     }, error => console.error(error));
+  }
+
+  public filter(topicId: number) {
+    this.topicId = topicId;
+    this.courses = topicId ? this.coursesAll.filter(c => c.topicId === topicId) : this.coursesAll;
   }
 }
 
