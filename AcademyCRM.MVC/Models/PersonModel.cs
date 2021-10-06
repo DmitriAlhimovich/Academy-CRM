f623a0848cf103e7a2cdec36f1869b7dae3a726f
Dmitriy Alhimovich <dmitriy.alhimovich@arcadia.spb.ru>
2021-10-06 10:59:21 +0300

    Show photo

--- AcademyCRM.MVC/Models/PersonModel.cs
+++ AcademyCRM.MVC/Models/PersonModel.cs
@@ -1,5 +1,4 @@
-﻿using AcademyCRM.MVC.Validation;
-using System;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace AcademyCRM.MVC.Models
@@ -19,7 +18,7 @@ namespace AcademyCRM.MVC.Models
         [StringLength(maximumLength: 100, MinimumLength = 3, ErrorMessage = "Length must be 3-100")]
         public string Email { get; set; }
 
-        public FileUpload FileUpload { get; set; }
+        public byte[] Photo { get; set; }
 
         public string FullName => $"{LastName} {FirstName}";
     }
